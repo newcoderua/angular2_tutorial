994b9e17fac5aa020fb05384be7b9e77aae7ced3
vladyslav <tradestadnyk@gmail.com>
2018-02-10 17:59:29 -0500

    simple angular2 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <user></user>
+  `,
 })
+
 export class AppComponent {
-  title = 'app';
+
 }
---
+++ src/app/components/user.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user',
+  template: `<h2>
+  {{name}}
+  <p>Your email is {{email}}</p>
+  </h2>
+  <p>State: {{address.state}}</p>
+  <p>City: {{address.city}}</p>
+  <p>Street: {{address.street}}</p>
+  <button (click)="toggleHobbies()">{{ showHobbies ? "Hide Hobbies" : "Show hobbies"}}</button>
+  <div *ngIf="showHobbies">
+    <h3>Hobbies:</h3>
+      <ul>
+        <li *ngFor="let hobby of hobbies">
+          {{hobby}}
+        </li>
+      </ul>
+      <form (submit)="addHobby(hobby.value)">
+        <label> Add Hobby: </label><br />
+        <input type="text" #hobby /><br />
+      </form>
+
+  </div>
+  <hr />
+  <form>
+    <label> Name: </label><br />
+    <input type="text" name="name" [(ngModel)]="name" /><br />
+    <label> Email: </label><br />
+    <input type="text" name="email" [(ngModel)]="email" /><br />
+    <label> State: </label><br />
+    <input type="text" name="address.state" [(ngModel)]="address.state" /><br />
+    <label> City: </label><br />
+    <input type="text" name="address.city" [(ngModel)]="address.city" />
+  </form>
+
+  `,
+
+})
+
+export class UserComponent {
+  name: string;
+  email: string;
+  address: address;
+  hobbies: string[];
+  showHobbies: boolean;
+
+  constructor(){
+    this.name = 'Vlad Stadnyk';
+    this.email = "vlad@gmail.com";
+    this.address = {
+      city: "NYC",
+      state: "NY",
+      street: "Broadway"
+    };
+    this.hobbies = ['Music', 'Soccer', 'Food']
+    this.showHobbies = false;
+  }
+
+  toggleHobbies(){
+    if(this.showHobbies === false) {
+      this.showHobbies = true;
+    } else {
+      this.showHobbies = false;
+    }
+  }
+
+  addHobby(hobby) {
+    console.log('test')
+  }
+
+}
+
+interface address {
+  street: string,
+  city: string,
+  state: string,
+}
